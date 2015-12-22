93e4d1837ad2cd92704c07004f5182a30707da1b
Vidur Murali <vyder@users.noreply.github.com>
2015-12-22 14:12:52 +0530

    But actually set tabsize to 2

--- interpreter.rb
+++ interpreter.rb
@@ -6,19 +6,20 @@ brainfuck = ARGV[0]
 
 index = 0
 brainfuck.split("").each do |char|
-	case char
-	when '+'
-		array[index] += 1
-	when '-'
-		array[index] -= 1
-	when '>'
-		index += 1
-	when '<'
-		index -= 1
-	when '.'
-		print array[index].chr
-	end
+  case char
+  when '+'
+    array[index] += 1
+  when '-'
+    array[index] -= 1
+  when '>'
+    index += 1
+  when '<'
+    index -= 1
+  when '.'
+    print array[index].chr
+  end
 end
 print "\n"
+
 
 # todo - loops
